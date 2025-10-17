8a6ad5bfda076f9193ca96e57206b0f3780ad37f
OscarFX <oscarfxs@gmail.com>
2025-10-16 23:25:27 -0300

    GET y fetch

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-console.log("Hola");
+//console.log(process.argv);
+
+let [, , method, resource] = process.argv;
+
+method = method.toUpperCase();
+resource = resource.toLowerCase();
+
+if (method == "GET" && resource == "products") {
+	//console.log("obtener productos");
+    fetch('https://fakestoreapi.com/products')
+  .then(response => response.json())
+  .then(data => console.log(data));
+}
+console.log("SE OBTUVIERON TODOS LOS PRODUCTOS");
