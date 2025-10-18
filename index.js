4d94581117d48f132c3c3a1153680cf13be94aff
OscarFX <oscarfxs@gmail.com>
2025-10-18 15:05:08 -0300

    GET products +id

--- index.js
+++ index.js
@@ -6,9 +6,29 @@ method = method.toUpperCase();
 resource = resource.toLowerCase();
 
 if (method == "GET" && resource == "products") {
-	//console.log("obtener productos");
-    fetch('https://fakestoreapi.com/products')
+	const response = await fetch('https://fakestoreapi.com/products')
+  const data = await response.json();
+  console.log(data);
+
+  //  fetch('https://fakestoreapi.com/products')
+  //.then(response => response.json())
+  //.then(data => console.log(data));
+}
+console.log("1ER REQUERIMIENTO - CONSULTAR TODOS LOS PRODUCTOS");
+
+// MOSTRAR IN ID EN PARTICULAR
+if (method == "GET" && resource.startsWith("products/")) {
+	let id = resource.split("/")[1];
+  id = parseInt(id);
+
+  if(isNaN(id) || id <=0) {
+    console.log("ERROR DE TIPEO - VERIFICAR-");
+  }
+
+  fetch("https://fakestoreapi.com/products/" + id)
   .then(response => response.json())
   .then(data => console.log(data));
+
 }
-console.log("SE OBTUVIERON TODOS LOS PRODUCTOS");
+console.log("2ER REQUERIMIENTO - CONSULTAR PRODUCTOS POR id");
+
