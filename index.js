c3fb148059601c5a9e5662b994573813efbfa5c5
OscarFX <oscarfxs@gmail.com>
2025-10-18 17:10:07 -0300

    GET, POST Y DELETE OK 2

--- index.js
+++ index.js
@@ -1,46 +1,31 @@
-//console.log(process.argv);
-
-let [, , method, resource, ...params] = process.argv; //
+let [, , method, resource, ...params] = process.argv;
 
 method = method.toUpperCase();
 resource = resource.toLowerCase();
 
+// MOSTRAR TODOS LOS PRODUCTOS/////////////////////////////////////
 if (method == "GET" && resource == "products") {
-	const response = await fetch('https://fakestoreapi.com/products')
+  const response = await fetch('https://fakestoreapi.com/products')
   const data = await response.json();
   console.log(data);
-
-  //  fetch('https://fakestoreapi.com/products')
-  //.then(response => response.json())
-  //.then(data => console.log(data));
-
   console.log("1ER REQUERIMIENTO - CONSULTAR TODOS LOS PRODUCTOS");
-}
 
-
-// MOSTRAR IN ID EN PARTICULAR////////////////////////////////////////////////
-if (method == "GET" && resource.startsWith("products/")) {
-	let id = resource.split("/")[1];
+// MOSTRAR UN PRODUCTO IDENTIFICADO POR SU ID //////////////////////
+} else if (method == "GET" && resource.startsWith("products/")) {
+  let id = resource.split("/")[1];
   id = parseInt(id);
 
   if(isNaN(id) || id <=0) {
     console.log("ERROR DE TIPEO - VERIFICAR-");
+  } else {
+    const response = await fetch("https://fakestoreapi.com/products/" + id)
+    const data = await response.json();
+    console.log(data);
+    console.log("2ER REQUERIMIENTO - CONSULTAR PRODUCTOS POR id");
   }
-  else
-  {
-  const response = await fetch("https://fakestoreapi.com/products/" + id)
-  const data = await response.json();
-  console.log(data);
- // fetch("https://fakestoreapi.com/products/" + id)
-  //.then(response => response.json())
-  //.then(data => console.log(data));
-  console.log("2ER REQUERIMIENTO - CONSULTAR PRODUCTOS POR id");
-  }
-}
-// AREGAR UN PRODUCTO ////////////////////////////////////////////////////////////
 
-if (method == "POST" && resource == "products") {
-
+//AGREGAR UN NUEVO PRODUCTO //////////////////////////////////////////////////
+} else if (method == "POST" && resource == "products") {
   const [title, price, category] = params;
 
   const product = {
@@ -56,25 +41,24 @@ if (method == "POST" && resource == "products") {
   })
     .then((response) => response.json())
     .then((data) => console.log(data));
-    console.log("3ER REQUERIMIENTO - AGREGAR UN PRODUCTO");
-}
+  console.log("3ER REQUERIMIENTO - AGREGAR UN PRODUCTO");
 
-// BORRAR UN PRODUCTO ESPECIFIFADO POR ID///////////////////////////////////////////////////////////////
-if (method == "DELETE" && resource.startsWith("products/")) {
+// ELIMINAR UN PRODUCTO IDENTIFICADO POR SU ID /////////////////////////////////  
+} else if (method == "DELETE" && resource.startsWith("products/")) {
   const id = parseInt(resource.split("/")[1]);
 
-   if(isNaN(id) || id <=0) {
+  if(isNaN(id) || id <=0) {
     console.log("ERROR DE TIPEO - VERIFICAR-");
-  }
-  else
-  {
-  fetch(`https://fakestoreapi.com/products/${id}`, {
-    method: "DELETE",
-  })
-    .then((response) => response.json())
-    .then((data) => console.log(data));
+  } else {
+    fetch(`https://fakestoreapi.com/products/${id}`, {
+      method: "DELETE",
+    })
+      .then((response) => response.json())
+      .then((data) => console.log(data));
 
-  console.log("4TOR REQUERIMIENTO - BORRAR UN PRODUCTO POR id");
+    console.log("4TOR REQUERIMIENTO - BORRAR UN PRODUCTO POR id");
   }
+} else {
+  console.log("No se encontró una coincidencia");
 }
 
